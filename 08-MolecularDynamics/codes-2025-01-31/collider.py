c77c7320f0bd64cd037a2a016491cd534448462b
iluvatar1 <woquendo@gmail.com>
2025-01-31 18:47:31 -0500

    Added first Molecular Dyanmics examples

---
+++ 08-MolecularDynamics/codes-2025-01-31/collider.py
@@ -0,0 +1,18 @@
+import numpy as np
+class Collider:
+    """
+    Class to compute forces on each body
+    """
+    # Parameters
+    G = np.array([0.0, -9.81, 0.0])
+    B = 3.9
+    K = 1000.345
+
+    # Functions
+    def computeForce(self, body): # For now operate on a single body
+        body.F = np.zeros(3) # Reset the force
+        body.F += body.mass*self.G # Add gravity
+        #body.F += -body.mass*self.B*body.V # -mbv , air friction
+        delta = body.rad - body.R[1]
+        if delta > 0:
+            body.F[1] += self.K*delta - body.mass*self.B*body.V[1]
